8799eea8c5f71355e3a1e117db28764bfdc95066
billalxcode <billal.xcode@gmail.com>
2024-09-10 08:36:25 +0700

    add modal filter and change background color

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -18,6 +18,8 @@ const StyledDialogOverlay = styled(AnimatedDialogOverlay)`
     display: flex;
     align-items: center;
     justify-content: center;
+    
+    backdrop-filter: blur(2px);
 
     background-color: ${({ theme }) => theme.modalBG};
   }
@@ -34,7 +36,7 @@ const StyledDialogContent = styled(({ minHeight, maxHeight, mobile, isOpen, ...r
   &[data-reach-dialog-content] {
     margin: 0 0 2rem 0;
     border: 1px solid ${({ theme }) => theme.bg1};
-    background-color: ${({ theme }) => theme.bg1};
+    background-color: ${({ theme }) => theme.bg2};
     box-shadow: 0 4px 8px 0 ${({ theme }) => transparentize(0.95, theme.shadow1)};
     padding: 0px;
     width: 50vw;
